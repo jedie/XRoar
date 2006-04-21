a28d7dd531313c43863bb7cf38af986fef757320
xroar <xroar@6809.org.uk>
2006-04-21 11:31:37 +0100

    Added sound_gp32.h
    darcs-hash:20060421103137-10d61-2a980dad624798ea077d9711e51d11b7212d03b6.gz

---
+++ sound_gp32.h
@@ -0,0 +1,11 @@
+/*  XRoar - a Dragon/Tandy Coco emulator
+ *  Copyright (C) 2003-2005  Ciaran Anscomb
+ *
+ *  See COPYING for redistribution conditions. */
+
+#ifndef __SOUND_GP32_H__
+#define __SOUND_GP32_H__
+
+void sound_silence(void);
+
+#endif  /* __SOUND_GP32_H__ */
