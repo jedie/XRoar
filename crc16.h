b7aeecf65ba8c6ad24e6aef23cabd3706b466d7c
xroar <xroar@6809.org.uk>
2007-03-09 14:21:42 +0000

    Change CRC algorithm
    Seems the CRC16 algorithm I found isn't the same as used by the controller
    (as discovered by analysing sample DMK disk images).  Now use big-endian
    "CRC-CCITT" 16-bit CRC with initial value of 0xffff.
    
    darcs-hash:20070309142142-10d61-3af1d0ccce4bf49b7cbb3d27dc66e82121e459bf.gz

--- crc16.h
+++ crc16.h
@@ -8,7 +8,9 @@
 
 #include "types.h"
 
-void crc16_reset(void);
+#define crc16_reset() crc16_set_value(0xffff)
+
+void crc16_set_value(uint16_t value);
 uint16_t crc16_value(void);
 void crc16_byte(uint8_t value);
 void crc16_block(uint8_t *block, int length);
