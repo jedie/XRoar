f94927d4d9ebc8ea5e26ca714be1be582c81ac19
xroar <xroar@6809.org.uk>
2008-03-25 18:36:57 +0000

    Reinstate event context into NDS sound patch
    darcs-hash:20080325183657-10d61-b4431be3c2b4429ff6dd14e27ef71bbd99428bc0.gz

--- sound_nds.c
+++ sound_nds.c
@@ -50,7 +50,7 @@ static Cycle frame_cycle_base;
 static Sample *wrptr;
 static Sample lastsample;
 
-static void flush_frame(void);
+static void flush_frame(void *context);
 static event_t *flush_event;
 
 static int init(int argc, char **argv) {
@@ -96,8 +96,9 @@ static void update(void) {
 	}
 }
 
-static void flush_frame(void) {
+static void flush_frame(void *context) {
 	Sample *fill_to = buffer + FRAME_SIZE;
+	(void)context;
 	while (wrptr < fill_to)
 		*(wrptr++) = lastsample;
 	frame_cycle_base += FRAME_CYCLES;
