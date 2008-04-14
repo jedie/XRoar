4e430d45b67be98b574649f2952676548de56f2a
xroar <xroar@6809.org.uk>
2008-04-14 19:33:18 +0100

    GP32: move GP32 specific defs into gp32/types.h
    darcs-hash:20080414183318-10d61-3cd3174f42633fe1401dd9b78ed6ff7922f73b36.gz

--- gp32/types.h
+++ gp32/types.h
@@ -42,4 +42,52 @@ typedef int ssize_t;
 
 typedef uint32_t Cycle __attribute__ ((aligned (4)));
 
+/* FCLK = 100/50/50MHz */
+/*
+#define SPEED_FAST GpClockSpeedChange(100000000, 0x2a011, 2)
+#define SPEED_SLOW GpClockSpeedChange(50000000, 0x2a012, 0)
+#define PCLK 50000000
+*/
+
+/* FCLK = 90/45/45MHz */
+/*
+#define SPEED_FAST GpClockSpeedChange(90000000, 0x25011, 2)
+#define SPEED_SLOW GpClockSpeedChange(45000000, 0x25012, 0)
+#define PCLK 45000000
+*/
+
+/* FCLK = 80/40/40MHz */
+
+#define SPEED_FAST GpClockSpeedChange(80000000, 0x48012, 2)
+#define SPEED_SLOW GpClockSpeedChange(40000000, 0x48013, 0)
+#define PCLK 40000000
+
+
+/* FCLK = 72/36/36MHz */
+/*
+#define SPEED_FAST GpClockSpeedChange(72000000, 0x28002, 2)
+#define SPEED_SLOW GpClockSpeedChange(36000000, 0x28003, 0)
+#define PCLK 36000000
+*/
+
+/* FCLK = 66/33/33MHz */
+/*
+#define SPEED_FAST GpClockSpeedChange(66000000, 0x24002, 2)
+#define SPEED_SLOW GpClockSpeedChange(33000000, 0x24003, 0)
+#define PCLK 33000000
+*/
+
+/* These are in the SDK but not declared: */
+void ARMEnableInterrupt(void);
+void ARMDisableInterrupt(void);
+void swi_install_irq(int irqnum, void *service_routine);
+void swi_uninstall_irq(int irqnum, void *service_routine);
+void swi_mmu_change(void *mempos_start, void *mempos_end, int mode);
+
+/* For use with swi_mmu_change: */
+#define MMU_NCNB (0xff2)        /* Noncached, nonbuffered */
+#define MMU_NCB (0xff6)         /* Noncached, buffered */
+#define MMU_WT (0xffa)          /* Cached write-through mode */
+#define MMU_WB (0xffe)          /* Cached write-back mode */
+
 #endif  /* __GP32_TYPES_H__ */
