b4351bd8fcaaaf2ae3f34549b8b70faa87826004
xroar <xroar@6809.org.uk>
2008-04-15 21:14:35 +0100

    Add nds/types.h
    darcs-hash:20080415201435-10d61-2dfa0c462bf8a202b7178b3b6b04c614e4b1f78e.gz

---
+++ nds/types.h
@@ -0,0 +1,14 @@
+/*  XRoar - a Dragon/Tandy Coco emulator
+ *  Copyright (C) 2003-2008  Ciaran Anscomb
+ *
+ *  See COPYING for redistribution conditions. */
+
+#ifndef __NDS_TYPES_H__
+#define __NDS_TYPES_H__
+
+#define puts(...)
+#define printf(...)
+#define sprintf siprintf
+#define snprintf sniprintf
+
+#endif  /* __NDS_TYPES_H__ */
