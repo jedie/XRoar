4bc1512827320dc7906c37a99b276f927b8511cf
xroar <xroar@6809.org.uk>
2008-04-24 09:22:57 +0100

    Make fast sound optional
    From configure and at runtime.
    
    darcs-hash:20080424082257-10d61-e27eb336db2413a715fe7835cf34b66b0688574e.gz

--- machine.c
+++ machine.c
@@ -83,6 +83,9 @@ MC6821_PIA PIA0, PIA1;
 
 Cycle current_cycle;
 int noextbas;
+#ifndef FAST_SOUND
+static int fast_sound;  /* don't update on control reg writes */
+#endif
 
 static const char *d32_extbas_roms[] = { "d32", "dragon32", "d32rom", "dragon", NULL };
 static const char *d64_extbas_roms[] = { "d64_1", "d64rom1", "dragrom", "dragon", NULL };
@@ -138,12 +141,18 @@ void machine_helptext(void) {
 	puts("  -ntsc                 emulate NTSC (60Hz) video");
 	puts("  -ccr RENDERER         specify cross-colour renderer (-ccr help for list)");
 	puts("  -ram KBYTES           specify amount of RAM in K");
+#ifndef FAST_SOUND
+	puts("  -fast-sound           faster but less accurate sound");
+#endif
 }
 
 void machine_getargs(int argc, char **argv) {
 	int i;
 	machine_clear_requested_config();
 	noextbas = 0;
+#ifndef FAST_SOUND
+	fast_sound = 0;
+#endif
 	for (i = 1; i < argc; i++) {
 		if (!strcmp(argv[i], "-machine") && i+1<argc) {
 			int j;
@@ -207,6 +216,11 @@ void machine_getargs(int argc, char **argv) {
 				}
 			}
 		}
+#ifndef FAST_SOUND
+		else if (!strcmp(argv[i], "-fast-sound")) {
+			fast_sound = 1;
+		}
+#endif
 	}
 }
 
@@ -241,16 +255,20 @@ void machine_init(void) {
 	mc6821_init(&PIA0);
 	PIA0.a.irq_line = PIA0.b.irq_line = &irq;
 	PIA0.a.data_postwrite = pia0a_data_postwrite;
-	PIA0.a.control_postwrite = pia0a_control_postwrite;
 	PIA0.b.data_postwrite = pia0b_data_postwrite;
-	PIA0.b.control_postwrite = pia0b_control_postwrite;
 	mc6821_init(&PIA1);
 	PIA1.a.irq_line = PIA1.b.irq_line = &firq;
 	PIA1.a.data_preread = pia1a_data_preread;
 	PIA1.a.data_postwrite = pia1a_data_postwrite;
 	PIA1.a.control_postwrite = pia1a_control_postwrite;
 	PIA1.b.data_postwrite = pia1b_data_postwrite;
-	PIA1.b.control_postwrite = pia1b_control_postwrite;
+#ifndef FAST_SOUND
+	if (!fast_sound) {
+		PIA0.a.control_postwrite = pia0a_control_postwrite;
+		PIA0.b.control_postwrite = pia0b_control_postwrite;
+		PIA1.b.control_postwrite = pia1b_control_postwrite;
+	}
+#endif
 	dragondos_init();
 	deltados_init();
 	rsdos_init();
