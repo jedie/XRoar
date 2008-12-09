2e06c4c415f6cb16d3528e7e476118d4b1f894e3
Ciaran Anscomb <xroar@6809.org.uk>
2008-12-09 21:10:24 +0000

    Better debugging for deltados
    darcs-hash:20081209211024-c25da-d2efbe31c21ed6777de1080c1530b5e050882e27.gz

--- deltados.c
+++ deltados.c
@@ -34,6 +34,7 @@
 #include "xroar.h"
 
 /* Latch that's part of the Delta cart: */
+static unsigned int ic1_old;
 static unsigned int ic1_drive_select;
 static unsigned int ic1_side_select;
 static unsigned int ic1_density;
@@ -50,6 +51,7 @@ void deltados_reset(void) {
 	wd279x_set_intrq_handler   = NULL;
 	wd279x_reset_intrq_handler = NULL;
 	wd279x_reset();
+	ic1_old = 0xff;
 	ic1_drive_select = 0xff;
 	ic1_side_select  = 0xff;
 	ic1_density      = 0xff;
@@ -74,22 +76,24 @@ void deltados_write(unsigned int addr, unsigned int val) {
 
 /* Delta cartridge circuitry */
 static void ff44_write(unsigned int octet) {
-	LOG_DEBUG(4, "Delta: Write to FF44: ");
-	if ((octet & 0x03) != ic1_drive_select) {
-		LOG_DEBUG(4, "DRIVE SELECT %01d, ", octet & 0x03);
+	if (octet != ic1_old) {
+		LOG_DEBUG(4, "Delta: Write to FF44: ");
+		if ((octet ^ ic1_old) & 0x03) {
+			LOG_DEBUG(4, "DRIVE SELECT %01d, ", octet & 0x03);
+		}
+		if ((octet ^ ic1_old) & 0x04) {
+			LOG_DEBUG(4, "SIDE %s, ", (octet & 0x04)?"1":"0");
+		}
+		if ((octet ^ ic1_old) & 0x08) {
+			LOG_DEBUG(4, "DENSITY %s, ", (octet & 0x08)?"DOUBLE":"SINGLE");
+		}
+		LOG_DEBUG(4, "\n");
+		ic1_old = octet;
 	}
-	if ((octet & 0x04) != ic1_side_select) {
-		LOG_DEBUG(4, "SIDE %s, ", (octet & 0x04)?"1":"0");
-	}
-	octet ^= 0x08;
-	if ((octet & 0x08) != ic1_density) {
-		LOG_DEBUG(4, "DENSITY %s, ", (octet & 0x08)?"SINGLE":"DOUBLE");
-	}
-	LOG_DEBUG(4, "\n");
 	ic1_drive_select = octet & 0x03;
 	vdrive_set_drive(ic1_drive_select);
 	ic1_side_select = octet & 0x04;
 	vdrive_set_side(ic1_side_select ? 1 : 0);
-	ic1_density = octet & 0x08;
+	ic1_density = !(octet & 0x08);
 	wd279x_set_density(ic1_density);
 }
