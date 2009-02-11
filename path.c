35222d0bbdd99f05a9649618633658af599692a1
Ciaran Anscomb <xroar@6809.org.uk>
2009-02-11 22:12:38 +0000

    Add missing path.c and path.h
    darcs-hash:20090211221238-c25da-8fbb08f4e63214a3172d3ccab08cd892ff06f021.gz

---
+++ path.c
@@ -0,0 +1,114 @@
+/*  XRoar - a Dragon/Tandy Coco emulator
+ *  Copyright (C) 2003-2009  Ciaran Anscomb
+ *
+ *  This program is free software; you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation; either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  This program is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with this program; if not, write to the Free Software
+ *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ */
+
+#include "config.h"
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <dirent.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#include "types.h"
+#include "path.h"
+
+static char *strcattoc_esc(char *dst, const char *src, char c);
+
+/* Find file within supplied colon-separated path.  In path elements, "~/" at
+ * the start is expanded to "$HOME/" and "\" escapes the following character
+ * (e.g., "\:" to stop a colon being seen as a path separator).
+ *
+ * Files are only considered if they are regular files (not sockets,
+ * directories, etc.) and are readable by the user.  This is not intended as a
+ * security check, just a convenience. */
+
+/* TODO: calls to stat() and access() need to be abstracted out into fs_*.c, as
+ * they will differ on GP32.  Perhaps roll them into one function that tests
+ * for readable regular file. */
+
+char *find_in_path(const char *path, const char *filename) {
+	struct stat statbuf;
+	const char *home;
+	char buf[MAXNAMLEN];
+
+	if (filename == NULL)
+		return NULL;
+	/* If no path or filename contains a directory, just test file */
+	if (path == NULL || strchr(filename, '/')) {
+		if (stat(filename, &statbuf) == 0)
+			if (statbuf.st_mode & S_IFREG)
+				if (access(filename, R_OK) == 0)
+					return strdup(filename);
+		return NULL;
+	}
+	home = getenv("HOME");
+	while (*path) {
+		*buf = 0;
+		/* Prefix $HOME if path elem starts "~/" */
+		if (home && *path == '~' && *(path+1) == '/') {
+			strcpy(buf, home);
+			path += 2;
+			if (buf[strlen(buf) - 1] != '/')
+				strcat(buf, "/");
+		}
+		/* Now append path element, "/" if required and the
+		 * filename */
+		strcattoc_esc(buf, path, ':');
+		if (*buf == 0)
+			strcpy(buf, "./");
+		else if (buf[strlen(buf) - 1] != '/')
+			strcat(buf, "/");
+		strcat(buf, filename);
+		/* Return this one if file is valid */
+		if (stat(buf, &statbuf) == 0) 
+			if (statbuf.st_mode & S_IFREG)
+				if (access(buf, R_OK) == 0)
+					return strdup(buf);
+		/* Skip to next path element */
+		while (*path && *path != ':') {
+			if (*path == '\\' && *(path+1) != 0)
+				path++;  /* skip escaped char */
+			path++;
+		}
+		if (*path == ':')
+			path++;
+	}
+	return NULL;
+}
+
+/* Helper function appends src to the end of dst until the first occurence of
+ * c.  "\" escapes the following character. */
+
+static char *strcattoc_esc(char *dst, const char *src, char c) {
+	char *ret = dst;
+	while (*dst != 0)
+		dst++;
+	while (*src && *src != c) {
+		if (*src == '\\') {
+			src++;
+			if (*src)
+				*(dst++) = *(src++);
+		} else {
+			*(dst++) = *(src++);
+		}
+	}
+	*dst = 0;
+	return ret;
+}
---
+++ path.h
@@ -0,0 +1,16 @@
+/*  XRoar - a Dragon/Tandy Coco emulator
+ *  Copyright (C) 2003-2009  Ciaran Anscomb
+ *
+ *  See COPYING.GPL for redistribution conditions. */
+
+#ifndef __PATH_H__
+#define __PATH_H__
+
+/* Try to find regular file within one of the directories supplied.  Returns
+ * allocated memory containing full path to file if found, NULL otherwise.
+ * Directory separator occuring within filename just causes that one file to be
+ * checked. */
+
+char *find_in_path(const char *path, const char *filename);
+
+#endif  /* __PATH_H__ */
