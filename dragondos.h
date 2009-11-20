0326ae9d59348c204e0af0320338cb350a00418e
Ciaran Anscomb <xroar@6809.org.uk>
2009-11-20 21:52:50 +0000

    Update copyright dates

--- dragondos.h
+++ dragondos.h
@@ -1,5 +1,5 @@
 /*  XRoar - a Dragon/Tandy Coco emulator
- *  Copyright (C) 2003-2008  Ciaran Anscomb
+ *  Copyright (C) 2003-2009  Ciaran Anscomb
  *
  *  See COPYING.GPL for redistribution conditions. */
 
