4db0138f2eb26d0527957656e3eea31c6151fa2f
Ciaran Anscomb <xroar@6809.org.uk>
2011-06-18 21:27:30 +0100

    jack: use new buffer management
    Can't currently seem to get *anything* working with jackd though,
    so this is untested.

--- jack/ao_jack.c
+++ jack/ao_jack.c
@@ -16,6 +16,8 @@
  *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
  */
 
+#include "config.h"
+
 #include <errno.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -25,45 +27,30 @@
 #include <pthread.h>
 
 #include "types.h"
-#include "events.h"
 #include "logging.h"
 #include "machine.h"
 #include "module.h"
-#include "types.h"
+#include "sound.h"
 #include "xroar.h"
 
 static int init(void);
 static void shutdown(void);
-static void update(int value);
-
 static void flush_frame(void);
+
 static int jack_callback(jack_nframes_t nframes, void *arg);
 
 SoundModule sound_jack_module = {
-	{ "jack", "JACK audio",
-	  init, 0, shutdown },
-	update
+	.common = { .name = "jack", .description = "JACK audio",
+		    .init = init, .shutdown = shutdown },
+	.flush_frame = flush_frame,
 };
-
-typedef jack_default_audio_sample_t Sample;
-
-static jack_nframes_t sample_rate;
-static jack_nframes_t frame_size;
-static Cycle sample_cycles;
-static Cycle frame_cycles;
-static int frame_cycle;
 
 static jack_client_t *client;
 static int num_output_ports;  /* maximum of 2... */
 static jack_port_t *output_port[2];
+static float *buffer;
 
-static Cycle frame_cycle_base;
-static Sample *buffer = NULL;
-static Sample *wrptr;
-static Sample lastsample;
 static pthread_mutex_t haltflag;
-
-static event_t *flush_event;
 
 static int init(void) {
 	const char **ports;
@@ -98,58 +85,26 @@ static int init(void) {
 	}
 	free(ports);
 	num_output_ports = i;
-	sample_rate = jack_get_sample_rate(client);
-	sample_cycles = OSCILLATOR_RATE / sample_rate;
-	frame_size = jack_get_buffer_size(client);
-	frame_cycles = sample_cycles * frame_size;
-	frame_cycle = 0;
-	buffer = malloc(frame_size * sizeof(Sample));
-	pthread_mutex_init(&haltflag, NULL);
-	flush_event = event_new();
-	flush_event->dispatch = flush_frame;
+	jack_nframes_t sample_rate = jack_get_sample_rate(client);
+	jack_nframes_t buffer_size = jack_get_buffer_size(client);
 
-	memset(buffer, 0, frame_size * sizeof(Sample));
-	wrptr = buffer;
-	frame_cycle_base = current_cycle;
-	flush_event->at_cycle = frame_cycle_base + frame_cycles;
-	event_queue(&MACHINE_EVENT_LIST, flush_event);
-	lastsample = 0.;
+	buffer = sound_init(sample_rate, 1, SOUND_FMT_FLOAT, buffer_size);
+	LOG_DEBUG(2, "\t%dms (%d samples) buffer\n", (buffer_size * 1000) / sample_rate, buffer_size);
+
+	pthread_mutex_init(&haltflag, NULL);
+
 	return 0;
 }
 
 static void shutdown(void) {
 	LOG_DEBUG(2,"Shutting down JACK audio driver\n");
 	pthread_mutex_destroy(&haltflag);
-	event_free(flush_event);
 	if (client)
 		jack_client_close(client);
 	client = NULL;
-	if (buffer)
-		free(buffer);
-	buffer = NULL;
-}
-
-static void update(int value) {
-	int elapsed_cycles = current_cycle - frame_cycle_base;
-	if (elapsed_cycles >= frame_cycles) {
-		elapsed_cycles = frame_cycles;
-	}
-	while (frame_cycle < elapsed_cycles) {
-		*(wrptr++) = lastsample;
-		frame_cycle += sample_cycles;
-	}
-	lastsample = (Sample)(value - 64) / 150.;
 }
 
 static void flush_frame(void) {
-	Sample *fill_to = buffer + frame_size;
-	while (wrptr < fill_to)
-		*(wrptr++) = lastsample;
-	frame_cycle_base += frame_cycles;
-	frame_cycle = 0;
-	flush_event->at_cycle = frame_cycle_base + frame_cycles;
-	event_queue(&MACHINE_EVENT_LIST, flush_event);
-	wrptr = buffer;
 	if (xroar_noratelimit)
 		return;
 	pthread_mutex_lock(&haltflag);
@@ -162,8 +117,8 @@ static int jack_callback(jack_nframes_t nframes, void *arg) {
 	jack_default_audio_sample_t *out;
 	(void)arg;  /* unused */
 	for (i = 0; i < num_output_ports; i++) {
-		out = (jack_default_audio_sample_t *)jack_port_get_buffer(output_port[i], nframes);
-		memcpy(out, buffer, sizeof(Sample) * nframes);
+		out = (float *)jack_port_get_buffer(output_port[i], nframes);
+		memcpy(out, buffer, nframes * sizeof(float));
 	}
 	pthread_mutex_unlock(&haltflag);
 	return 0;
