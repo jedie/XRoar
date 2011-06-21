f172b9ee26cde69d84520a79211c9a7f08bad6c0
Ciaran Anscomb <xroar@6809.org.uk>
2011-06-21 18:28:16 +0100

    Update FSF address
    Thanks to Andrea Musuruane.

--- gtk2/ui_gtk2.c
+++ gtk2/ui_gtk2.c
@@ -13,7 +13,7 @@
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
+ *  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
  */
 
 #include <stdio.h>
@@ -309,7 +309,7 @@ static void about(GtkMenuItem *item, gpointer data) {
 "\n"
 "You should have received a copy of the GNU General Public License\n"
 "along with this program; if not, write to the Free Software\n"
-"Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA"
+"Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 	);
 	gtk_about_dialog_set_website(dialog, "http://www.6809.org.uk/dragon/xroar.shtml");
 	g_signal_connect(dialog, "response", G_CALLBACK(close_about), NULL);
