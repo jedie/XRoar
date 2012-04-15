db9301361060d14d5dd9508f66ddde12b1199705
Ciaran Anscomb <xroar@6809.org.uk>
2012-04-15 21:37:03 +0100

    Use more compact CRC-CCITT

--- crc16.h
+++ crc16.h
@@ -1,7 +1,6 @@
-/*  XRoar - a Dragon/Tandy Coco emulator
- *  Copyright (C) 2003-2012  Ciaran Anscomb
- *
- *  See COPYING.GPL for redistribution conditions. */
+/* CRC-16-CCITT with bytes processed high bit first ("big-endian"), as used in
+ * the WD279X FDC (polynomial 0x1021).  In the FDC, CRC is initialised to
+ * 0xffff and NOT inverted before appending to the message. */
 
 #ifndef XROAR_CRC16_H_
 #define XROAR_CRC16_H_
@@ -10,7 +9,7 @@
 
 #define CRC16_RESET (0xffff)
 
-uint16_t crc16_byte(uint16_t crc, uint8_t value);
-uint16_t crc16_block(uint16_t crc, uint8_t *block, int length);
+uint16_t crc16_byte(uint16_t crc, uint8_t value) __attribute__ ((pure));
+uint16_t crc16_block(uint16_t crc, uint8_t *block, unsigned int length);
 
 #endif  /* XROAR_CRC16_H_ */
