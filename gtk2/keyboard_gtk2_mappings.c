23f78d369d5135d28fe7b58d8b56117476a34268
Ciaran Anscomb <xroar@6809.org.uk>
2012-05-06 08:14:16 +0100

    Big copyright notice update

--- gtk2/keyboard_gtk2_mappings.c
+++ gtk2/keyboard_gtk2_mappings.c
@@ -1,3 +1,21 @@
+/*  Copyright 2003-2012 Ciaran Anscomb
+ *
+ *  This file is part of XRoar.
+ *
+ *  XRoar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  XRoar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XRoar.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 /* The Dragon keyboard layout:
  *
  *   1   2   3   4   5   6   7   8   9   0   :   -  brk
--- vo_generic_ops.c
+++ vo_generic_ops.c
@@ -1,7 +1,20 @@
-/*  XRoar - a Dragon/Tandy Coco emulator
- *  Copyright (C) 2003-2012  Ciaran Anscomb
+/*  Copyright 2003-2012 Ciaran Anscomb
  *
- *  See COPYING.GPL for redistribution conditions. */
+ *  This file is part of XRoar.
+ *
+ *  XRoar is free software: you can redistribute it and/or modify
+ *  it under the terms of the GNU General Public License as published by
+ *  the Free Software Foundation, either version 2 of the License, or
+ *  (at your option) any later version.
+ *
+ *  XRoar is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *  GNU General Public License for more details.
+ *
+ *  You should have received a copy of the GNU General Public License
+ *  along with XRoar.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 /* This file contains generic scanline rendering routines.  It is included
  * into various video module source files and makes use of macros defined in
