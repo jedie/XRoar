acbfb7e148510b1bcf89ef082d75921a1de00bec
Ciaran Anscomb <xroar@6809.org.uk>
2012-10-16 09:30:55 +0100

    portalib: update notices in musl code
    Be clear that these come from the last LGPL-licensed release of musl.

--- portalib/strcasecmp.c
+++ portalib/strcasecmp.c
@@ -1,8 +1,13 @@
-/* Functions taken from from musl libc, © 2005-2011 Rich Felker. */
+/* Taken from musl libc v0.8.10, © 2005-2012 Rich Felker. */
+
+/* This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2 of the License, or (at your option) any later version.
+ */
 
 #include "config.h"
 
-#include <stdlib.h>
 #include <ctype.h>
 
 #include "portalib/strings.h"
