2dbb5766c0d74208df6547760ebd629a3caba771
Ciaran Anscomb <xroar@6809.org.uk>
2013-08-21 19:04:56 +0100

    pl_glib: add g_ascii_strcasecmp functions

--- portalib/pl_glib/gstrfuncs.c
+++ portalib/pl_glib/gstrfuncs.c
@@ -13,3 +13,37 @@ gchar *g_strdup(const gchar *str) {
 	gsize len = strlen(str);
 	return g_memdup(str, len + 1);
 }
+
+gint g_ascii_strcasecmp(const gchar *s1, const gchar *s2) {
+	if (!s1 || !s2)
+		return 0;
+	while (*s1 && *s2 && (*s1 == *s2 || g_ascii_tolower(*s1) == g_ascii_tolower(*s2))) {
+		s1++;
+		s2++;
+	}
+	return g_ascii_tolower(*s1) - g_ascii_tolower(*s2);
+}
+
+gint g_ascii_strncasecmp(const gchar *s1, const gchar *s2, gsize n) {
+	if (!s1 || !s2)
+		return 0;
+	while (n > 0 && *s1 && *s2 &&
+	       (*s1 == *s2 || g_ascii_tolower(*s1) == g_ascii_tolower(*s2))) {
+		s1++;
+		s2++;
+		n--;
+	}
+	return (n == 0) ? 0 : g_ascii_tolower(*s1) - g_ascii_tolower(*s2);
+}
+
+gchar g_ascii_tolower(gchar c) {
+	if (g_ascii_isupper(c))
+		return c | 0x20;
+	return c;
+}
+
+gchar g_ascii_toupper(gchar c) {
+	if (g_ascii_islower(c))
+		return c & ~0x20;
+	return c;
+}
--- portalib/pl_glib/gstrfuncs.h
+++ portalib/pl_glib/gstrfuncs.h
@@ -6,5 +6,17 @@
 #include "pl_glib/gtypes.h"
 
 gchar *g_strdup(const gchar *str);
+gint g_ascii_strcasecmp(const gchar *s1, const gchar *s2);
+gint g_ascii_strncasecmp(const gchar *s1, const gchar *s2, gsize n);
+gchar g_ascii_tolower(gchar c);
+gchar g_ascii_toupper(gchar c);
+
+static inline gboolean g_ascii_islower(gchar c) {
+	return (c >= 'a' && c <= 'z');
+}
+
+static inline gboolean g_ascii_isupper(gchar c) {
+	return (c >= 'A' && c <= 'Z');
+}
 
 #endif  /* def PORTALIB_GLIB_GSTRFUNCS_H_ */
