14c575260a8265de09527fd269f1ac44553351bc
Ciaran Anscomb <xroar@6809.org.uk>
2013-08-22 11:29:40 +0100

    portalib: document pl_string.h, don't include string.h
    Users need to include both.

--- portalib/pl_string.h
+++ portalib/pl_string.h
@@ -1,12 +1,24 @@
-#ifndef PORTALIB_STRING_H_
-#define PORTALIB_STRING_H_
+/*
+ * pl_string.h supplies prototypes for string utility functions that were found
+ * not to be supported by the target's libc.
+ *
+ * It is still necessary to define the appropriate feature macro (e.g.,
+ * _BSD_SOURCE) before inclusion to get the right set of prototypes.
+ *
+ * Of course there's only one function here so far, so that doesn't mean much.
+ */
+
+#ifndef PL_STRING_H_
+#define PL_STRING_H_
 
 #include "config.h"
 
-#include <string.h>
+#ifdef _BSD_SOURCE
 
-#if defined(NEED_STRSEP) && defined(_BSD_SOURCE)
+#ifdef NEED_STRSEP
 char *strsep(char **, const char *);
 #endif
 
-#endif  /* PORTALIB_STRING_H_ */
+#endif
+
+#endif  /* PL_STRING_H_ */
