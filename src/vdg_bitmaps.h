1b16f05685956941cf6313def8982d821867693e
Ciaran Anscomb <xroar@6809.org.uk>
2013-09-18 09:38:38 +0100

    font2c: don't output stuff that can change
    Keeps messing with files in version control.

--- src/vdg_bitmaps.h
+++ src/vdg_bitmaps.h
@@ -1,10 +1,10 @@
 /* Automatically generated
- * by tools/font2c from ./src/font-6847.png */
+ * by ../tools/font2c from /Users/ciarana/src/xroar/src/font-6847.png */
 
 extern const unsigned char font_6847[768];
 
 /* Automatically generated
- * by tools/font2c from ./src/font-6847t1.png */
+ * by ../tools/font2c from /Users/ciarana/src/xroar/src/font-6847t1.png */
 
 extern const unsigned char font_6847t1[1536];
 
